bd0a1c85d9c04afca36d624c39316cf74862ef17
zueunjoeng <judms0314@gmail.com>
2024-07-04 13:08:15 +0900

    헤더 fonts,css수정

--- src/component/Hd.js
+++ src/component/Hd.js
@@ -1,13 +1,14 @@
 import logo from '../img/logo.svg'
+import hdscss from '../scss/hd.module.scss'
 
 function Hd() {
     return (
-        <header className='border-bottom hd_font'>
+        <header className={hdscss.hd_font}>
             <div className="d-flex align-items-center m-0 me-5 ms-5">
                 <h1 className='pt-1'><a href=""><img src="/img/logo.png" alt="" /></a></h1>
                 {/* <h1><a href=""><img src={logo} alt="" /></a></h1> */}  
-                <p className='m-0 ms-2'>가능성에 <span>무한함</span>을</p>
-                <ul className="d-flex pt-1 me-0 ms-auto">
+                <p className='m-0 ms-2 fw300'>가능성에 <span className='fw900'>무한함</span>을</p>
+                <ul className="d-flex pt-1 me-0 ms-auto fw300">
                     <li className='me-3'><a href="">ABOUT ME</a></li>
                     <li className='me-3'><a href="">PROJECT</a></li>
                     <li className='me-3'><a href="">TEAMPROJECT</a></li>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Jeong from './Introduce';
+import './css/fonts.css'
 
 
 
